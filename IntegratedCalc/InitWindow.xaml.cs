e858488957b3756b19894bbb063f2ea5d29d97e3
ProphetLamb <rene.carannante@outlook.de>
2020-11-13 13:50:45 +0100

    Fix only partially load settings on launch

--- IntegratedCalc/InitWindow.xaml.cs
+++ IntegratedCalc/InitWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace IntegratedCalc
     /// </summary>
     public partial class InitWindow : Window
     {
-        private SettingsProvider<SettingsObject> _defaultProvider;
+        private SettingsProvider<SettingsObject> _provider;
         public InitWindow()
         {
             InitializeComponent();
@@ -23,29 +23,33 @@ namespace IntegratedCalc
             var serializer = new Newtonsoft.Json.JsonSerializer();
             serializer.Formatting = Newtonsoft.Json.Formatting.Indented;
             serializer.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
-            _defaultProvider = new SettingsProvider<SettingsObject>("settings.json", serializer, null);
-            if (!File.Exists("settings.json"))
-            {
-                _defaultProvider.SaveAsync().ConfigureAwait(false);
-            }
-            SettingsManager.Add(_defaultProvider);
+            _provider = new SettingsProvider<SettingsObject>("settings.json", serializer, null);
+            SettingsManager.Add(_provider);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            ExceptionExtentions.Try(() =>
+            {
+                if (!_provider.Get())
+                {
+                    _provider.Current = SettingsObject.Default;
+                    _provider.SaveAsync().ConfigureAwait(false);
+                }
+            });
             Dispatcher.BeginInvoke(new Action(() =>
             {
                 StickyWindow.RegisterExternalReferenceForm(this);
                 Hide();
             }), null);
-            var mw = new MainWindow(_defaultProvider);
+            var mw = new MainWindow(_provider);
             mw.Closed += Mw_Closed;
             mw.Show();
         }
 
         private void Mw_Closed(object sender, EventArgs e)
         {
-            SaveSettings(_defaultProvider);
+            SaveSettings(_provider);
             SettingsManager.Clear();
             Close();
         }
@@ -61,6 +65,7 @@ namespace IntegratedCalc
             // Duplicates are removed, when trying to add the commands, but still the json is getting spammed, so here I select the entries with a distinct cmdlet to remove duplicates.
             // Distinct doesnt work either whyever, and it needs a equalitycomparer :(. So I made my own distinct extention using a equalitycomparison which WORKs, tho its literally a copy and paste from the equalitycomparer.... Microsoft pls fix!
             provider.Current.WebCommands = provider.Current.WebCommands.Distinct((x, y) => x.Cmdlet.Equals(y.Cmdlet, StringComparison.InvariantCultureIgnoreCase)).ToList();
+            provider.Current.LaunchCommands = provider.Current.LaunchCommands.Distinct((x, y) => x.Cmdlet.Equals(y.Cmdlet, StringComparison.InvariantCultureIgnoreCase)).ToList();
             provider.Save();
         }
     }
