9ed7911359722094db76ed414cbd03fc463fd71e
ProphetLamb <rene.carannante@outlook.de>
2020-11-13 23:36:41 +0100

    Fix incorrect loading of settings

--- IntegratedCalc/InitWindow.xaml.cs
+++ IntegratedCalc/InitWindow.xaml.cs
@@ -49,24 +49,14 @@ namespace IntegratedCalc
 
         private void Mw_Closed(object sender, EventArgs e)
         {
-            SaveSettings(_provider);
+            _provider.Save();
             SettingsManager.Clear();
             Close();
         }
 
         internal static async Task SaveSettingsAsync(SettingsProvider<SettingsObject> provider)
         {
-            await Task.Run(() => SaveSettings(provider));
-        }
-
-        internal static void SaveSettings(SettingsProvider<SettingsObject> provider)
-        {
-            // The deserializer duplicates webcmdlets once everytime its loads settings.json, I cant figure out the reason why or where
-            // Duplicates are removed, when trying to add the commands, but still the json is getting spammed, so here I select the entries with a distinct cmdlet to remove duplicates.
-            // Distinct doesnt work either whyever, and it needs a equalitycomparer :(. So I made my own distinct extention using a equalitycomparison which WORKs, tho its literally a copy and paste from the equalitycomparer.... Microsoft pls fix!
-            provider.Current.WebCommands = provider.Current.WebCommands.Distinct((x, y) => x.Cmdlet.Equals(y.Cmdlet, StringComparison.InvariantCultureIgnoreCase)).ToList();
-            provider.Current.LaunchCommands = provider.Current.LaunchCommands.Distinct((x, y) => x.Cmdlet.Equals(y.Cmdlet, StringComparison.InvariantCultureIgnoreCase)).ToList();
-            provider.Save();
+            await Task.Run(provider.Save);
         }
     }
 }
